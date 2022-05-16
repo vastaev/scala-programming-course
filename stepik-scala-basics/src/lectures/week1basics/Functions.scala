d48bf9f49bdf5b4065f256eb6bb7b5b261bfc8b5
Concetta Joanne <cjoanne@ge-g4.kzn.21-school.ru>
2022-05-16 18:14:03 +0300

    done  types, strings, expressions, functions

---
+++ stepik-scala-basics/src/lectures/week1basics/Functions.scala
@@ -0,0 +1,16 @@
+package lectures.week1basics
+
+object Functions extends App {
+  def callByValue(x: Long): Unit = {
+    println(s"call by value: x1 = $x")
+    println(s"call by value: x2 = $x")
+  }
+
+  def callByName(x: => Long): Unit = {
+    println(s"call by name: x1 = $x")
+    println(s"call by name: x2 = $x")
+  }
+
+  callByValue(System.nanoTime())
+  callByName(System.nanoTime())
+}
